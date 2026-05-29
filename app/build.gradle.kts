576fbdb404bbfcde05d4dc27a03128505b2027ff
yuaxi <yuaxi0806@gmail.com>
2026-05-29 11:36:33 +0800

    upload LICENSE and change version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.flammedemon.ts6droid"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0-Han"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
